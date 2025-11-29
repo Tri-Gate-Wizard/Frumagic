80925e26493ac3a87e44a91d4a37d7e2c8117bff
Tri-Gate-Wizard <syami1000@outlook.jp>
2025-11-29 17:07:45 +0900

    BattleManager軽く完成。この後ボタンを押したらターンが進むようにする。

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {   
+    public Player player;
+    public Enemy enemy;
     bool playerTurn = true;
     int turnCount = 0;
+    bool battlecontinues = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,7 +28,7 @@ public class BattleManager : MonoBehaviour
     }
     void TurnManager()
     {   
-        bool battlecontinues = true;
+        
         playerTurn = true;
         while(battlecontinues == true)
         {
@@ -37,39 +40,45 @@ public class BattleManager : MonoBehaviour
             {
                 EnemyTurn();
             }
+            Debug.Log(turnCount + "ターン目終了");
             turnCount++;
+            
             if (turnCount >= 10)
             {
-                Debug.Log("Battle ended in a draw due to turn limit.");
+                Debug.Log("時間切れ…");
                 battlecontinues = false;
             }
         }
-        
-        
-
-        // Code to initiate battle sequence
+        Debug.Log("バトル終了！");
 
     }
     void PlayerTurn()
     {
-        Debug.Log("Make your move!");
+        Debug.Log("行動を選択してね!");
         string input = "";  // Placeholder for player input
-        input = Console.ReadLine();
+        //input = Console.ReadLine();
+        input = "attack"; //仮の入力
         if (input == "attack")
         {
-            Debug.Log("You attacked the enemy!");
-            // Code to handle attack
+            Debug.Log(enemy.name + "に攻撃!");
+            enemy.Damage(player.Atk);
+            if(!enemy.IsAlive())
+            {
+                Debug.Log(enemy.name + " の負け!");
+                // Code to handle enemy defeat
+                battlecontinues = false;
+            }
             playerTurn = false;
         }
         else if (input == "defend")
         {
-            Debug.Log("You defended against the enemy's attack!");
+            Debug.Log("防御成功!");
             // Code to handle defense
             playerTurn = false;
         }
         else
         {
-            Debug.Log("Invalid move. Try again.");
+            Debug.Log("よくわからんかったわ。もう一回言ってくれ");
         }
     }
 
@@ -77,7 +86,14 @@ public class BattleManager : MonoBehaviour
     {
         Debug.Log("Enemy's turn!");
         // Code for enemy action
-        Debug.Log("The enemy attacked you!");
+        Debug.Log(enemy.name + "の攻撃!");
+        player.Damage(enemy.Atk);
+        if(!player.IsAlive())
+        {
+            Debug.Log("あなたの負け!");
+            // Code to handle player defeat
+            battlecontinues = false;
+        }
         playerTurn = true;
     }
 }
