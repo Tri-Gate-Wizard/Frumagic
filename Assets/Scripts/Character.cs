31ae304abc71ff9dab0402b382a992a9826654f1
Tri-Gate-Wizard <syami1000@outlook.jp>
2025-11-29 16:38:45 +0900

    Characterファイルを作成。プレイヤーやMobの虎の子になるクラスが書いてある。

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int HP;
+    public int Atk;
+
+    public bool IsAlive()
+    {
+        return (HP > 0);
+    }
+
+    public void Damage(int damage)
+    {
+        Debug.Log(this.name + "は" + damage + "ダメージ受けた!");
+        HP -= damage;
+        if (HP <= 0)
+            HP = 0;
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HP = 100;
+        Atk = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
