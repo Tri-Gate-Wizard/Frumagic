2ab21f0d6e3a28f372eb85109113402ffe2d7121
Tri-Gate-Wizard <syami1000@outlook.jp>
2025-12-14 19:09:03 +0900

    ファイル移動は面倒だからやめてよねぇー！！

--- Assets/Scripts/BattleStarter.cs
+++ Assets/Scripts/BattleStarter.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleStarter: MonoBehaviour
 {
+    public List<Enemy> enemieList;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
