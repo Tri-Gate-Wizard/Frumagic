2a9e263a96b8b23efc8b07920a2ead56eafb35fb
Tri-Gate-Wizard <syami1000@outlook.jp>
2026-02-28 21:05:16 +0900

    いろいろだるかったのでいったんランダム生成やめました。

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,10 +3,6 @@ using UnityEngine.Tilemaps;
 public class MapGenerator : MonoBehaviour
 {
     public Tilemap tilemap;
-    public TileBase groundTile; 
-    public TileBase wallTile;
-    public TileBase TransitionTile;
-    public GameObject TransitionObject;
 
     
     public float wallProbability = 0.2f;
@@ -19,32 +15,11 @@ public class MapGenerator : MonoBehaviour
     }
     void MapGenerate()
     {
-        mapMemoryKeeper.mapData = new int[mapMemoryKeeper.width, mapMemoryKeeper.height];
-
-        for (int x = 0; x < mapMemoryKeeper.width; x++)
-        {
-            for (int y = 0; y < mapMemoryKeeper.height; y++)
-            {
-                mapMemoryKeeper.mapData[x, y] = (Random.value < wallProbability) ? 1 : 0;
-                
-            }
-        }
         
     }
     void SettingTile()
     {
-        for (int x = 0; x < mapMemoryKeeper.width; x++)
-        {
-            for (int y = 0; y < mapMemoryKeeper.height; y++)
-            {
-                Vector3Int tilePos = new Vector3Int(x, y, 0);
-                TileBase tile = (mapMemoryKeeper.mapData[x, y] == 1) ? wallTile : groundTile;
-                tilemap.SetTile(tilePos, tile);
-            }
-        }
-        Vector3Int transpos = new Vector3Int(Random.Range(0, mapMemoryKeeper.width), Random.Range(0, mapMemoryKeeper.height),0);
-        tilemap.SetTile(transpos, TransitionTile);
-        TransitionObject.transform.position = transpos;
+        
     }
 
     // Update is called once per frame
